37e18cc4defcc3ff511ed744c439f3fa689ba6d8
Matt Mallett <mmallett91@gmail.com>
2020-11-11 22:01:53 -0500

    checkpoint

---
+++ script.js
@@ -0,0 +1,19 @@
+const FAIR_START_DATE = 0;//1606626000000;
+const FAIR_END_DATE = 1607317200000;
+
+$(document).ready(showFair);
+
+function init() {
+    $('#fair-page').hide();
+    $('#enter').hide();
+    const now = Date.now();
+    if (now >= FAIR_START_DATE && now <= FAIR_END_DATE) {
+        $('#enter').show();
+    }
+    $('#enter').click(showFair);
+}
+
+function showFair() {
+    $('#landing-page').hide();
+    $('#fair-page').show();
+}
