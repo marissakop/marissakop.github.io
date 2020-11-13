e2fc53c60ca389bbdf7f33c6840fcb942518e6cb
Matt Mallett <mmallett91@gmail.com>
2020-11-12 22:44:48 -0500

    add countdown

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
-const FAIR_START_DATE = 0;//1606626000000;
+const FAIR_START_DATE = 1606626000000;
 const FAIR_END_DATE = 1607317200000;
 
 $(document).ready(showLanding);
 
+let interval;
 function showLanding() {
     $('#fair-page').hide();
     $('#enter').hide();
+    interval = setInterval(checkTime, 1000);
+    checkTime();
+}
+
+function checkTime() {
     const now = Date.now();
     if (now >= FAIR_START_DATE && now <= FAIR_END_DATE) {
+        $('#countdown').hide();
         $('#enter').show();
+        $('#enter').click(showFair);
+        clearInterval(interval);
+        return;
     }
-    $('#enter').click(showFair);
+
+    const total = FAIR_START_DATE - now;
+    const seconds = Math.floor( (total/1000) % 60 );
+    const minutes = Math.floor( (total/1000/60) % 60 );
+    const hours = Math.floor( (total/(1000*60*60)) % 24 );
+    const days = Math.floor( total/(1000*60*60*24) );
+    if (days > 0) {
+        $('#countdown').text(`Fair opens in ${days} days, ${hours} hours, check back soon!`);
+    } else {
+        $('#countdown').text(`Fair opens in ${hours} hours, ${minutes} minutes, ${seconds} seconds, check back soon!`);
+    }
 }
 
 function showFair() {
