2f615f7151a084a8c56e42824f5ca564f0af1c76
Matt Mallett <mmallett91@gmail.com>
2020-11-28 18:15:22 -0500

    pics updated
    pics updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const FAIR_START_DATE = 1606626000000;
 const FAIR_END_DATE = 1607317200000;
 
-$(document).ready(showLanding);
+$(document).ready(showFair);
 
 let interval;
 function showLanding() {
